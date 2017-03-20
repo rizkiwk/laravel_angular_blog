e269b5d3d394eb94455dfcd41f123d15ba331b19
Rizki Wais Kurniawan <develop.rizkiwk@gmail.com>
2017-03-21 06:21:21 +0700

    commit- config db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel_angular_blog'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'iowzksiyw6nglkjh'),
+            'password' => env('DB_PASSWORD', 't4brc14gjdv2rsm9'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
